90ff7052cf7b948805772cf23ddea66951d02cd6
Dilermando Barbosa Jr <dilermando.barbosa@gmail.com>
2017-12-05 16:21:24 -0200

    Testes melhores

--- DesafioSomaDoisApp/DesafioSomaDoisController.swift
+++ DesafioSomaDoisApp/DesafioSomaDoisController.swift
@@ -14,6 +14,7 @@ class DesafioSomaDoisController {
         
         // codigo aqui
         
+        
         return false
     }
     
--- DesafioSomaDoisAppTests/DesafioSomaDoisAppTests.swift
+++ DesafioSomaDoisAppTests/DesafioSomaDoisAppTests.swift
@@ -12,20 +12,22 @@ import XCTest
 class DesafioSomaDoisAppTests: XCTestCase {
     
     func testSomaDois() {
-        self.measure {
-            let numeros = [2,3,6,8,8,14,16,25,32,44,45,57,59,60,62,64,68,70,71,100,260,1505,2000,2200,2450,2607]
-            XCTAssertTrue(DesafioSomaDoisController().conseguimosAcharONumero(31, somandoDoisNumerosDaLista: numeros))
-            XCTAssertFalse(DesafioSomaDoisController().conseguimosAcharONumero(4, somandoDoisNumerosDaLista: numeros))
-        }
+
+        let numeros = [2,3,6,8,8,14,17,25,32,44,45,57,59,60,62,64,68,70,71,100,260,1505,2000,2200,2450,2607]
+        XCTAssertTrue(DesafioSomaDoisController().conseguimosAcharONumero(16, somandoDoisNumerosDaLista: numeros))
+        XCTAssertFalse(DesafioSomaDoisController().conseguimosAcharONumero(2606, somandoDoisNumerosDaLista: numeros))
+    
     }
     
-    func testPerformance() {
+    func testTodosOsCenarios() {
         var numeros: [Int] = []
         self.measure {
             for i in 0..<5000 {
                 numeros.append(i*3)
             }
-            XCTAssertTrue(DesafioSomaDoisController().conseguimosAcharONumero(12, somandoDoisNumerosDaLista: numeros))
+            XCTAssertTrue(DesafioSomaDoisController().conseguimosAcharONumero(15003, somandoDoisNumerosDaLista: numeros))
+            XCTAssertFalse(DesafioSomaDoisController().conseguimosAcharONumero(29996, somandoDoisNumerosDaLista: numeros))
+            XCTAssertTrue(DesafioSomaDoisController().conseguimosAcharONumero(3, somandoDoisNumerosDaLista: numeros))
         }
     }
     
