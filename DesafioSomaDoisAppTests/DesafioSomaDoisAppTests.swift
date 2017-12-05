a91d09fb4d48f50a948ddca976a31ac573c17c97
Dilermando Barbosa Jr <dilermando.barbosa@gmail.com>
2017-12-05 11:30:08 -0200

    criado novo teste para validar performance do código.

--- DesafioSomaDoisAppTests/DesafioSomaDoisAppTests.swift
+++ DesafioSomaDoisAppTests/DesafioSomaDoisAppTests.swift
@@ -12,11 +12,20 @@ import XCTest
 class DesafioSomaDoisAppTests: XCTestCase {
     
     func testSomaDois() {
-        
         self.measure {
             let numeros = [2,3,6,8,8,14,16,25,32,44,45,57,59,60,62,64,68,70,71,100,260,1505,2000,2200,2450,2607]
             XCTAssertTrue(DesafioSomaDoisController().conseguimosAcharONumero(31, somandoDoisNumerosDaLista: numeros))
             XCTAssertFalse(DesafioSomaDoisController().conseguimosAcharONumero(4, somandoDoisNumerosDaLista: numeros))
+        }
+    }
+    
+    func testPerformance() {
+        var numeros: [Int] = []
+        self.measure {
+            for i in 0..<5000 {
+                numeros.append(i*3)
+            }
+            XCTAssertTrue(DesafioSomaDoisController().conseguimosAcharONumero(12, somandoDoisNumerosDaLista: numeros))
         }
     }
     
